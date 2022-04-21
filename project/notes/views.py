f8d90d54858fc6069248fe40cc4ecc4477ea382e
paurbx <gleroapachii@gmail.com>
2022-04-22 02:27:41 +0300

    95% done

--- project/notes/views.py
+++ project/notes/views.py
@@ -15,21 +15,12 @@ class IndexPage(ListView):
         context = super().get_context_data(**kwargs)
         context['form'] = CreateNoteForm
         context['complited'] = Note.objects.filter(status=True)
-        print(context)
         return context
-    # def get(self, request, *args, **kwargs):
-    #     form = CreateNoteForm()
-    #     complited = Note.objects.filter(status=True)
-    #     return render(request, self.template_name, {'form': form, 'notes': Note.objects.all(), 'complited': complited})
 
     def post(self, request, *args, **kwargs):
         form = CreateNoteForm(request.POST or None)
         if form.is_valid():
-            header = form.cleaned_data.get('header')
-            text = form.cleaned_data.get('text')
-            status = form.cleaned_data.get('status')
-            new_note = Note.objects.create(header=header, text=text, status=status)
-            new_note.save()
+            form.save()
             return redirect('index_page')
 
 
@@ -42,18 +33,30 @@ class DeleteNote(DeleteView):
     success_url = reverse_lazy('index_page')
     template_name = 'confirm_delete_note.html'
 
+class DeleteComplited(ListView):
+    queryset = Note.objects.filter(status = True)
+    template_name = 'confirm_delete_completed.html'
+    success_url = reverse_lazy('index_page')
+
+    def get(self, request, *args, **kwargs):
+        queryset = Note.objects.filter(status=True)
+        return render(request, self.template_name, {'notes': queryset})
+    def post(self, request, *args, **kwargs):
+        queryset = Note.objects.filter(status=True).delete()
+        return redirect(self.success_url)
+
 class UpdateNote(UpdateView):
     model = Note
     form_class = UpdateNoteForm
     template_name = 'update_note.html'
     success_url = reverse_lazy('index_page')
 
+# ПЕРЕПИСАТЬ НА КЛАСС
 def change_status(request, pk):
     note = Note.objects.get(id=pk)
     note.change_status()
     note.save()
     return redirect('/')
 
-# ПЕРЕПИСАТЬ НА КЛАСС
 
 
